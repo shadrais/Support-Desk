1c5c876aa596bd5811a96ec9acec98a163477f6d
Shad Rais <shadrais321@outlook.com>
2022-10-10 14:13:49 +0000

    Closed Ticket Fixed

--- frontend/src/features/ticket/ticketService.js
+++ frontend/src/features/ticket/ticketService.js
@@ -1,52 +1,57 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl = 'https://3000-shadrais-supportdesk-p1zndkfksd9.ws-us70.gitpod.io/api/tickets'
+const baseUrl =
+  "https://3000-shadrais-supportdesk-p1zndkfksd9.ws-us70.gitpod.io/api/tickets";
 
 const createTicket = async (ticketData, token) => {
   const config = {
     headers: {
       authorization: `Bearer ${token}`,
     },
-  }
-  const response = await axios.post(baseUrl, ticketData, config)
-  return response.data
-}
+  };
+  const response = await axios.post(baseUrl, ticketData, config);
+  return response.data;
+};
 
 const getTickets = async (token) => {
   const config = {
     headers: {
       authorization: `Bearer ${token}`,
     },
-  }
-  const response = await axios.get(baseUrl, config)
-  return response.data
-}
+  };
+  const response = await axios.get(baseUrl, config);
+  return response.data;
+};
 
-const getTicket = async(id,token) => {
+const getTicket = async (id, token) => {
   const config = {
     headers: {
       authorization: `Bearer ${token}`,
     },
-  }
-  const response = await axios.get(baseUrl + '/'+ id, config)
-  return response.data
-}
+  };
+  const response = await axios.get(baseUrl + "/" + id, config);
+  return response.data;
+};
 
-const closeTicket = async(id,token) => {
+const closeTicket = async (id, token) => {
   const config = {
     headers: {
       authorization: `Bearer ${token}`,
     },
-  }
-  const response = await axios.put(baseUrl + '/' + id, {status: 'Close'}, config)
-  return response.data
-}
+  };
+  const response = await axios.put(
+    baseUrl + "/" + id,
+    { status: "Closed" },
+    config
+  );
+  return response.data;
+};
 
 const ticketApi = {
   createTicket,
   getTickets,
   getTicket,
-  closeTicket
-}
+  closeTicket,
+};
 
-export default ticketApi
+export default ticketApi;
