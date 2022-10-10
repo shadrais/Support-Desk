d7295efdd746d4189100b340b90eb5ddd5e37d01
Shad Rais <shadrais321@outlook.com>
2022-10-10 16:21:05 +0000

    Prod Deploy

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,33 +1,33 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = 'https://3000-shadrais-supportdesk-p1zndkfksd9.ws-us70.gitpod.io/api/users/'
+const API_URL = "https://brown-beaver-slip.cyclic.app/api/users/";
 
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData)
+  const response = await axios.post(API_URL, userData);
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+    localStorage.setItem("user", JSON.stringify(response.data));
   }
-  console.log(response.data)
-  return response.data
-}
+  console.log(response.data);
+  return response.data;
+};
 
 const logout = () => {
-  localStorage.removeItem('user')
-}
+  localStorage.removeItem("user");
+};
 
 const login = async (userData) => {
-  const response = await axios.post(API_URL + 'login', userData)
+  const response = await axios.post(API_URL + "login", userData);
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+    localStorage.setItem("user", JSON.stringify(response.data));
   }
-  console.log(response.data)
-  return response.data
-}
+  console.log(response.data);
+  return response.data;
+};
 
 const authApi = {
   register,
   logout,
   login,
-}
+};
 
-export default authApi
+export default authApi;
